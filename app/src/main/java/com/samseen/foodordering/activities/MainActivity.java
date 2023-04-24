a3ab0b511634552c07067fd87b9369abc9459d3c
Samson Akanbi <samson.akanbi@outlook.com>
2023-04-24 21:58:44 +0100

    Setting up the recyclerview.

--- app/src/main/java/com/samseen/foodordering/activities/MainActivity.java
+++ app/src/main/java/com/samseen/foodordering/activities/MainActivity.java
@@ -1,16 +1,28 @@
 package com.samseen.foodordering.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.samseen.foodordering.R;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewCategoryList, recyclerViewPopularList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList = findViewById(R.id.view1);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
     }
 }
